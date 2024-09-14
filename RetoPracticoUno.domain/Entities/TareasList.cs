f2990d59f14917b3a55f5aba514b16b87e6ae9b2
danny <dannyquesacascante@gmail.om>
2024-09-14 15:14:27 +0000

    tareaslist

---
+++ RetoPracticoUno.domain/Entities/TareasList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoPracticoUno.domain.Entities
+{
+    public class TareasList
+    {
+
+        List<Tarea>? Tareas { get; set; }
+
+    }
+}
