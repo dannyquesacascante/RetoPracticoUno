f20582843a7ea5ccaf85c56aef2342b3f9d4476c
danny <dannyquesacascante@gmail.om>
2024-09-14 15:06:19 +0000

    Add project files.

---
+++ RetoPracticoUno.domain/Entities/Tarea.cs
@@ -0,0 +1,12 @@
+﻿namespace RetoPracticoUno.domain.Entities
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public enum Estado { Completo, NoCompleto }
+
+    }
+
+}
